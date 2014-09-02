7a5339b8f07b25ba15d9062724509300f2325e2f
nemo9955 <nemo9955@gmail.com>
2014-09-02 16:57:21 +0300

    added libGDX project

---
+++ desktop/src/com/nemo9955/starting_fire/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nemo9955.starting_fire.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nemo9955.starting_fire.StartingFire;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StartingFire(), config);
+	}
+}
---
+++ html/src/com/nemo9955/starting_fire/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.nemo9955.starting_fire.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.nemo9955.starting_fire.StartingFire;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new StartingFire();
+        }
+}
